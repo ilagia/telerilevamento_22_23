4c652ed926946d688fda960b8f4a3252189d3e49
ilagia <100956875+ilagia@users.noreply.github.com>
2022-04-01 16:49:51 +0200

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -57,6 +57,16 @@ par(mfrow=c(2,1))
 plot(ndvi1992, col=cl)
 plot(ndvi2006, col=cl)
 
+#install packages RStoolbox
+install.packages("RStoolbox")
+library(RStoolbox)
+si1992 <- spectralIndices(l1992,green=3, red=2, nir=1)
+plot(si1992, col=cl)
+plot(copndvi)
+
+
+
+
 
 
 
